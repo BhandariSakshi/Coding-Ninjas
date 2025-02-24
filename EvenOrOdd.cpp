eb812ed36e9d330023d0b2026619ae64514f0c98
Bhandari Sakshi <125456744+BhandariSakshi@users.noreply.github.com>
2025-02-24 21:54:55 +0530

    Create EvenOrOdd.cpp

---
+++ EvenOrOdd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int num;
+	cin>>num;
+
+	if(num & 1)
+	{
+		cout << "Odd" << endl;
+	}
+	else
+	{
+		cout << "Even" << endl;
+	}
+	
+}
