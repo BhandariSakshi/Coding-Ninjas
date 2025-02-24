806622dc855b82c4ae3092209264f54aad157184
Bhandari Sakshi <125456744+BhandariSakshi@users.noreply.github.com>
2025-02-24 21:51:41 +0530

    Create 1ToNWithoutLoop.cpp

---
+++ 1ToNWithoutLoop.cpp
@@ -0,0 +1,12 @@
+vector<int> printNos(int x) {
+    // Write Your Code Here
+    
+    if (x==0)
+    {
+        return {};
+    }
+vector<int>result=printNos(x-1);
+result.push_back(x);
+    
+    return result;
+}
